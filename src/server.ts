ecf4547c619846ccb47b4c8bbb5a150c733a1f14
samuel <samuelalejandro.dev@gmail.com>
2023-06-05 22:55:26 -0300

    Criado upload de arquivos

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,26 @@ import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories.js'
 import { authRoutes } from './routes/auth.js'
 import jwt from '@fastify/jwt'
+import { uploadRoutes } from './routes/upload.js'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(require('@fastify/multipart'))
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
 })
-
 app.register(jwt, {
   secret: 'spacetime',
 })
 
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 app.register(authRoutes)
 
 app
